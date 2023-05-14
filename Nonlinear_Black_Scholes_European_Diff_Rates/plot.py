acd655f02c9065294572215f7a1976a1e3c2424a
Ayoub HAIDA <ayoub.haida369@gmail.com>
2023-05-14 03:49:27 +0100

    Initial commit

---
+++ Nonlinear_Black_Scholes_European_Diff_Rates/plot.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the training history data
+history = np.loadtxt('./logs/test_training_history.csv', delimiter=',', skiprows=1)
+
+# Extract the relevant columns
+epochs = history[:, 0]
+loss = history[:, 1]
+yval = history[:, 2]
+
+
+# Create the figure and axes
+fig, ax = plt.subplots(1, 2, figsize=(15, 6))
+
+# Plot the loss
+color = 'tab:blue'
+ax[0].semilogy(epochs, loss, color=color)
+ax[0].set_xlabel('$n_{epoch}$')
+ax[0].set_ylabel('Loss')
+ax[0].set_title('Loss')
+
+# Plot the value y=u(0,x)
+ax[1].plot(epochs, yval, color=color)
+ax[1].set_xlabel('$n_{epoch}$')
+ax[1].set_ylabel('$y=u(0,x)$')
+ax[1].set_title('Value $u(0,x)$')
+
+plt.savefig('logs/stats_plot( B-S_Eur_Diff_Rates ).png')
+
+# Show the plot
+plt.show()
+
---
+++ PDE_Quadratically_Growing_Derivatives/PDE_Quadratically_Growing_Derivatives_dim100.py
@@ -0,0 +1,64 @@
+"""
+The main file to run BSDE solver to solve parabolic partial differential equations (PDEs).
+
+"""
+
+import json
+import munch
+import os
+import logging
+
+from absl import app
+from absl import flags
+from absl import logging as absl_logging
+import numpy as np
+import tensorflow as tf
+
+import equation as eqn
+from solver import BSDESolver
+
+
+flags.DEFINE_string('config_path', 'configs/Parameters.json',
+                    """The path to load json file.""")
+flags.DEFINE_string('exp_name', 'test',
+                    """The name of numerical experiments, prefix for logging""")
+FLAGS = flags.FLAGS
+FLAGS.log_dir = './logs'  # directory where to write event logs and output array
+
+
+def main(argv):
+    del argv
+    with open(FLAGS.config_path) as json_data_file:
+        config = json.load(json_data_file)
+    config = munch.munchify(config)
+    bsde = getattr(eqn, config.eqn_config.eqn_name)(config.eqn_config)
+    tf.keras.backend.set_floatx(config.net_config.dtype)
+
+    if not os.path.exists(FLAGS.log_dir):
+        os.mkdir(FLAGS.log_dir)
+    path_prefix = os.path.join(FLAGS.log_dir, FLAGS.exp_name)
+    with open('{}_config.json'.format(path_prefix), 'w') as outfile:
+        json.dump(dict((name, getattr(config, name))
+                       for name in dir(config) if not name.startswith('__')),
+                  outfile, indent=2)
+
+    absl_logging.get_absl_handler().setFormatter(logging.Formatter('%(levelname)-6s %(message)s'))
+    absl_logging.set_verbosity('info')
+
+    logging.info('Begin to solve %s ' % config.eqn_config.eqn_name)
+    bsde_solver = BSDESolver(config, bsde)
+    training_history = bsde_solver.train()
+    if bsde.y_init:
+        logging.info('Y0_true: %.4e' % bsde.y_init)
+        logging.info('relative error of Y0: %s',
+                     '{:.2%}'.format(abs(bsde.y_init - training_history[-1, 2])/bsde.y_init))
+    np.savetxt('{}_training_history.csv'.format(path_prefix),
+               training_history,
+               fmt=['%d', '%.5e', '%.5e', '%d'],
+               delimiter=",",
+               header='step,loss_function,target_value,elapsed_time',
+               comments='')
+
+
+if __name__ == '__main__':
+    app.run(main)
